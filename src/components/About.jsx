07a2afd95a92432870e3a4238a3236ae640c6f02
arian <arian.shakib77@gmail.com>
2023-05-17 14:32:05 +0330

    Start of About Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default About
+export default About;
